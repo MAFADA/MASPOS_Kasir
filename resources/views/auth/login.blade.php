0a006ab84689ee436644e041acf23c3f6372945e
M. AFADA NUR SAIVA SYAHIRA <afadanursaiva2001@gmail.com>
2024-10-24 13:08:12 +0700

    Register Link

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,7 +32,15 @@
             </label>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
+
+
+        <div class="flex items-center space-x-4 justify-end mt-4">
+            @if (Route::has('register'))
+                <a href="{{ route('register') }}"
+                    class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    Register
+                </a>
+            @endif
             @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                     href="{{ route('password.request') }}">
